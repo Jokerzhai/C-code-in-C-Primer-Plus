20d14c9fce74119cd9a693e2940f35fbfd2fa63e
Joker Zhai(nxf45038) <zhiyu.zhai@nxp.com>
2018-12-24 14:13:41 +0800

    first commit

---
+++ two_func/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+void butler(void)£»
+int main()
+{
+    cout << "I will summon the butler function" << endl;
+    butler();
+    cout << "Hello world!" << endl;
+    return 0;
+}
